a1abc4cb4434e5f2088462d7903f2fa2afa69ac1
Michael Adams <mtadams007@gmail.com>
2018-04-18 10:41:06 -0400

    fixing classes and started cakes page

--- app.rb
+++ app.rb
@@ -4,6 +4,13 @@ require 'sendgrid-ruby'
 require_relative "classes.rb"
 include SendGrid
 
+$carrot = Cake.new("Carrot Cake", "A tasty cake made with organic carrots imported from Mbabane, Swaziland!", '$450', "/images/carrot_cake.jpeg")
+
+$vanilla = Cake.new("Vanilla Cake", "A delicious cake made with imported vanilla from Ranomafana, Madagascar!", '$500', "/images/vanilla_cake.jpeg")
+
+$strawberry = Cake.new("Strawberry Cake", "A scrumptious cake made with strawberries from Mongolia!", '$400', "/images/strawberry_cake.jpeg")
+
+
 get '/' do
   erb :index
 end
--- classes.rb
+++ classes.rb
@@ -1,10 +1,37 @@
 class Pastry
+  attr_reader :type
+  def initialize(type)
+    @type = type
+  end
+end
+
+class Muffin < Pastry
   attr_reader :description, :price, :pic
   def initialize(description, price, pic)
+    super("muffin")
     @description = description
     @price = price
     @pic = pic
   end
 end
 
-class 
+class Cake < Pastry
+  attr_reader :description, :price, :pic, :name
+  def initialize(name, description, price, pic)
+    super("cake")
+    @name = name
+    @description = description
+    @price = price
+    @pic = pic
+  end
+end
+
+class Cookie < Pastry
+  attr_reader :description, :price, :pic
+  def initialize(description, price, pic)
+    super("cookie")
+    @description = description
+    @price = price
+    @pic = pic
+  end
+end
